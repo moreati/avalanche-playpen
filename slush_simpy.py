6480ac07b0b069c8c8b886130cba45c7a5afd9a1
Alex Willmer <alex@moreati.org.uk>
2018-06-26 01:01:42 +0100

    Add a monitor process, run in realtime

--- slush_simpy.py
+++ slush_simpy.py
@@ -48,14 +48,23 @@ class Node:
                     self.color = color
             yield self.env.timeout(1)
 
+def monitor(env, nodes):
+    # TODO Overwriting only works when the output fits on a single line
+    # TODO Does this run at the end of a step? beginning? indeterminate?
+    while True:
+        print(f'{env.now:3d}', end=' ')
+        print(''.join(f'{node.color.value:d}' for node in nodes), end='\r')
+        yield env.timeout(1)
+
 
 if __name__ == '__main__':
-    env = simpy.Environment()
+    env = simpy.rt.RealtimeEnvironment()
     initial_colors = [Color(random.randint(0, 2)) for i in range(num_nodes)]
     nodes = [Node(env, color) for color in initial_colors]
     print('BEGIN')
     print(''.join('%i' % node.color.value for node in nodes))
     procs = [env.process(node.run(nodes, sample_size)) for node in nodes]
+    env.process(monitor(env, nodes))
     env.run(until=num_rounds)
     print('END')
     print(''.join('%i' % node.color.value for node in nodes))
