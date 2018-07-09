fe2c1b2b57e6fd04f119a2b7e7277bb2ac44752e
Alex Willmer <alex@moreati.org.uk>
2018-07-09 21:35:55 +0100

    Print node colors using curtsies

--- slush_simpy.py
+++ slush_simpy.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import enum
+import itertools
 import random
 
+import curtsies
 import simpy
 
 
@@ -18,6 +20,11 @@ sample_threshold = 0.6     # α in the whitepaper
 
 assert sample_size < num_nodes
 assert 0.5 < sample_threshold < 1.0
+
+
+def grouper(n, iterable, padvalue=None):
+    "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
+    return itertools.zip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
 
 
 class Color(enum.Enum):
@@ -54,23 +61,34 @@ class SlushNode:
                     self.color = color
             yield self.env.timeout(1)
 
+
+def node_portrayal(node):
+    bg = ['black', 'red', 'blue'][node.color.value]
+    return curtsies.fmtstr(' ', bg=bg)
+
+
 def monitor(env, nodes):
-    # TODO Overwriting only works when the output fits on a single line
     # TODO Does this run at the end of a step? beginning? indeterminate?
-    while True:
-        print(f'{env.now:3d}', end=' ')
-        print(''.join(f'{node.color.value:d}' for node in nodes), end='\r')
-        yield env.timeout(1)
+    with curtsies.CursorAwareWindow() as win:
+        while True:
+            lines = grouper(win.width, nodes)
+            vis = curtsies.fsarray([
+                curtsies.fmtstr('').join(
+                    node_portrayal(node) for node in line if node)
+                for line in lines
+            ])
+            win.render_to_terminal(vis)
+            yield env.timeout(delay=1)
 
 
 if __name__ == '__main__':
     env = simpy.rt.RealtimeEnvironment()
     initial_colors = [Color(random.randint(0, 2)) for i in range(num_nodes)]
     nodes = [SlushNode(env, color) for color in initial_colors]
-    print('BEGIN')
-    print(''.join('%i' % node.color.value for node in nodes))
+    #print('BEGIN')
+    #print(''.join('%i' % node.color.value for node in nodes))
     procs = [env.process(node.run(nodes, sample_size)) for node in nodes]
     env.process(monitor(env, nodes))
     env.run(until=num_rounds)
-    print('END')
-    print(''.join('%i' % node.color.value for node in nodes))
+    #print('END')
+    #print(''.join('%i' % node.color.value for node in nodes))
